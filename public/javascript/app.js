1a3824cb11a536c326955b6a9891508ae8af5cf6
KC Baeten <kcbaeten@KCs-MacBook-Pro.local>
2016-08-15 14:38:08 -0600

    updating structure

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -4,31 +4,44 @@ app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'partials/landing.html',
-      controller: 'landingController'
+      controller: 'mainController'
     })
     .when('/info', {
       templateUrl: 'partials/info.html',
-      controller: 'infoController'
+      controller: 'mainController'
     })
-    .when('/mockup', {
-      templateUrl: 'partials/mockup.html',
-      controller: 'mockUpController'
+    .when('/userstories', {
+      templateUrl: 'partials/userstories.html',
+      controller: 'mainController'
+    })
+    .when('/wireframes', {
+      templateUrl: 'partials/wireframes.html',
+      controller: 'mainController'
     })
     .otherwise({
       redirectTo: '/'
     });
 }]);
 
-app.controller('landingController', function($scope){
+app.controller('mainController', function($scope, $http){
   $scope.view = {};
-  $scope.view.message = "Welcome!"
+  $scope.stories = function() {
+    $http({
+      method: "GET",
+      url: "http://www.pivotaltracker.com/services/v5/projects/1786315/stories",
+      headers: {'X-TrackerToken': 'a7906909bb1350ebcd24558a1edbab3e'}
+    })
+    .then(function(data){
+      console.log(data.data);
+      $scope.view.results = data.data.stories;
+      console.log($scope.view.results);
+    });
+  };
 });
 
-app.controller('infoController', function($scope){
-  $scope.view = {};
-  $scope.view.message = "Welcome!"
-});
-app.controller('mockUpController', function($scope){
-  $scope.view = {};
-  $scope.view.message = "Welcome!"
-});
+app.directive('landing', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'public/partials/landing.html'
+  }
+})
