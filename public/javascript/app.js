16c565521c5a64076697f03a1b1dc769c0c71169
KC Baeten <kcbaeten@KCs-MacBook-Pro.local>
2016-08-19 16:07:26 -0600

    styling user stories

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,40 +3,50 @@ var app = angular.module("orderZipDemo", ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'partials/landing.html',
-      controller: 'mainController'
+      templateUrl: 'partials/landing.html'
     })
     .when('/info', {
-      templateUrl: 'partials/info.html',
-      controller: 'mainController'
+      templateUrl: 'partials/info.html'
     })
     .when('/userstories', {
       templateUrl: 'partials/userstories.html',
-      controller: 'mainController'
+      controller: 'storiesController'
     })
     .when('/wireframes', {
-      templateUrl: 'partials/wireframes.html',
-      controller: 'mainController'
+      templateUrl: 'partials/wireframes.html'
     })
     .otherwise({
       redirectTo: '/'
     });
 }]);
 
-app.controller('mainController', function($scope, $http){
+app.controller('storiesController', function($scope, $http){
   $scope.view = {};
-  $scope.stories = function() {
-    $http({
-      method: "GET",
-      url: "https://www.pivotaltracker.com/services/v5/projects/1786315/stories",
-      headers: {'X-TrackerToken': 'a7906909bb1350ebcd24558a1edbab3e'}
-    })
-    .then(function(data){
-      console.log(data.data);
-      $scope.view.results = data.data.stories;
-      console.log($scope.view.results);
-    });
-  };
+  $scope.view.storyIds = [];
+  $scope.view.tasks = [];
+  $http({
+    method: "GET",
+    url: "https://www.pivotaltracker.com/services/v5/projects/1786315/stories",
+    headers: {'X-TrackerToken': 'a7906909bb1350ebcd24558a1edbab3e'}
+  })
+  .then(function(data){
+    $scope.view.stories = data.data;
+    for (var i = 0; i < $scope.view.stories.length; i++) {
+      $scope.view.storyIds.push($scope.view.stories[i].id);
+    }
+    for (var i = 0; i < $scope.view.storyIds.length; i++) {
+      console.log("Hello");
+      $http({
+        method: "GET",
+        url: "https://www.pivotaltracker.com/services/v5/projects/1786315/stories/" + $scope.view.storyIds[i] + "/tasks",
+        headers: {'X-TrackerToken': 'a7906909bb1350ebcd24558a1edbab3e'}
+      })
+      .then(function(data){
+        $scope.view.tasks.push[i] = data.data;
+      });
+    }
+  });
+  console.log($scope.view);
 });
 
 app.directive('landing', function() {
